28b33ca72573fe144234754779e4ab78327a9f2c
Sumanth Narem <120869569+danger-3003@users.noreply.github.com>
2024-02-19 19:53:55 +0530

    Add files via upload

---
+++ SpeechRecognition.py
@@ -0,0 +1,174 @@
+import speech_recognition as S#converting voice to speech
+import pyttsx3 as P#converting text to speech
+import urllib#for opening the url's
+import pyautogui as pg#for doing interacting with the system
+import pywhatkit as pw#for opening youtube and other websites
+import keyboard as kb#for clicking keyboard keys
+import datetime as dt#for getting the date and time
+import pyjokes#for access the jokes
+from time import sleep#for giving a delay between actions
+import os#for interacting with os
+from playsound import playsound#used to play sound
+
+recogniser=S.Recognizer()#accessing the recogniser from speech_recognition module
+
+engine=P.init()#initialising the enigne from pyttsx3 module 
+voice=engine.getProperty('voices')
+engine.setProperty('voice',voice[0].id)#voice[0] for male and voice[1] for female
+engine.setProperty('rate',130)#adjusting the speed of the voice
+
+def speak(command):
+    engine.say(command)#converts the command into speech
+    engine.runAndWait()#wait for some time after speaking
+
+def connected():#check if you are connected to internet or not
+    try:
+        urllib.request.urlopen('https://www.google.com',timeout=2)
+        return True
+    except urllib.error.URLError as error:
+        return False
+    except:
+        return False
+
+#function that converts speech to text
+def voice_command_processor():
+    with S.Microphone() as source:#accessing the primary microphone as the source
+        recogniser.adjust_for_ambient_noise(source,duration=0.2)
+        playsound("sounds/open.wav")
+        audio = recogniser.listen(source,phrase_time_limit=4)#listening continuously to the speech for 4 seconds
+        text = ''#creating empty string to store the text converted form speech
+        try:
+            print("recognizing....")
+            text=recogniser.recognize_google(audio)#using google speech recognition
+            playsound("sounds/close.wav")
+            text=text.lower()#converting the text to lowercase
+        except S.UnknownValueError as e:
+            print(e)#printing the error if occured any
+        except S.RequestError as e:
+            speak("service is down.")
+            print("service is down")
+            return("service is down")
+        return text.lower()
+    
+#function to search for something
+def search(command):
+    string=""
+    if "search for" in command:#looking for "search for" key in the given command        
+        string=command.split("search for")#exactly splitting the command at "search for" key
+        """
+            command : could you please search for bommalu
+            output : ['could you please','bommalu']
+            string[1] : bommalu  --- accessing the 2nd element from the list
+        """
+        speak(f'Searching {string[1]}')
+        pw.search(string[1])
+    elif "search" in command:
+        #same like above, instead "search" is used
+        string=command.split("search")
+        speak(f"Searching {string[1]}")
+        pw.search(string[1])
+
+class time_date():
+    time=dt.datetime.now().strftime("%I %M %p")#to get time --- %I : hrs, %M : minutes, %p : seconds
+    time_24=dt.datetime.now().strftime("%H")#to get hours in 24h format
+    date=dt.datetime.now().strftime("%d %B %Y")#to get date --- %d : Date, %B : Month, %Y : Year
+    day=dt.datetime.now().strftime("%A")#to get day
+    today = dt.date.today()
+    y_date = today-dt.timedelta(days=1)#subtracting 1 day from current date to get yesterday's date
+    t_date = today+dt.timedelta(days=1)#adding 1 day to current date to get tomorrow's date
+    dy_date = today-dt.timedelta(days=2)#subtracting 2 days from current date to get day before yesterday's date
+    dt_date = today+dt.timedelta(days=2)#adding 2 days from current date to get day after tomorrow's date
+obj=time_date()
+
+def play(command):
+    song=command.split("play")#exactly splitting the command at "play" key
+    """
+        command : could you please play bavalu song
+        output : ['could you please','bavalu song']
+        song[1] : bavalu song  --- accessing the 2nd element from the list
+    """
+    if song!="":
+        print("playing "+song[1])
+        speak("playing"+song[1])
+        pw.playonyt(song[1])#open youtube and search for the song
+        kb.press("space")#press enter to play the song
+
+def open(command):
+    command = command.split("open")#exactly splitting the command at "open" key
+    """
+        command : could you please open chrome
+        output : ['could you please','chrome']
+        command[1] : chrome  --- accessing the 2nd element from the list
+    """
+    speak(f'opening {command[1]}')
+    pg.press("win")#press the win key
+    sleep(0.5)
+    kb.write(command[1])#enter the command
+    sleep(0.5)
+    kb.press("enter")#click enter
+
+def y_t_date(command):
+    if "today's" in command or "today" in command:
+        print(obj.date)
+        speak("today's date is "+str(obj.date))
+    elif "day after tomorrow" in command or "day after tomorrow's" in command:
+        print(obj.dt_date)
+        speak("day after tomoerrow's date is "+str(obj.dt_date))
+    elif "day before yesterday" in command or "day before yesterday's" in command:
+        print(obj.dy_date)
+        speak("day before yesterday's date is "+str(obj.dy_date))
+    elif "yesterday's" in command or "yesterday" in command:
+        print(obj.y_date)
+        speak("yesterday's date is "+str(obj.y_date))
+    elif "tomorrow's" in command or "tomorrow" in command:
+        print(obj.t_date)
+        speak("tomorrow's date is "+str(obj.t_date))
+    elif "date" in command:
+        print(obj.date)
+        speak("today's date is "+str(obj.date))
+
+def executable(command):
+    num=[2,1]
+    if "your name" in command or "who are you" in command:
+        speak("My name is Elite, i'm your voice assistant")
+        print("My name is The Elite, i'm your voice assistent")
+    elif command=="":
+        pass
+    elif "time" in command:
+        print(obj.time)
+        speak("the time is "+str(obj.time))
+    elif "date" in command:
+        y_t_date(command)
+    elif "joke" in command and "tell" in command:
+        joke=pyjokes.get_joke()
+        print(joke)
+        speak(joke)
+    elif "open" in command:
+        open(command)
+    elif "play" in command:
+        play(command)
+    elif "search" in command:
+        search(command)
+    elif "bye" in command or "good bye" in command:
+        speak("Bye")
+    elif "shutdown" in command:
+        speak("The system will Shutdown in, 3")
+        playsound("sounds/beep.wav")
+        for i in num:
+            speak(i)
+            playsound("sounds/beep.wav")
+            if i==1:
+                print(True)
+                #os.system("shutdown /s /t 5")
+    elif "restart" in command or "restart the system" in command:
+        speak("The system will restart in, 3")
+        playsound("sounds/beep.wav")
+        for i in num:
+            speak(i)
+            playsound("sounds/beep.wav")
+            if i==1:
+                print(True)
+                #os.system("shutdown /r /t 5")
+    elif "logout" in command or "logout from the system" in command:
+        speak("Logging out")
+        os.system("shutdown /l /t 0")
---
+++ new.py
@@ -0,0 +1,116 @@
+
+'''
+the architecture of the  mediapipe module for hand detection be like...
+
+mediapipe --- main module
+code: import mediapipe
+
+solutions --- provides a suite of libraries for detecting hands, face, bodymarks for better predection using ML & AI algorithms
+code: mediapipe.solutions
+
+hands --- submodule for hands in suite of libararies
+code: mediapipe.solutions.hands
+
+Hands() --- class to detect hands in the given input
+code: mediapipe.solutions.hands.Hands(<parameters>)
+
+HandLandmark[--point_name--] --- used to access each point on the hand, --point_name-- for identification like THUMB_TIP, INDEX_FINGER_TIP etc..
+code: mediapipe.solutions.hands.HandLandmark[--point_name--]
+
+process(image) --- used to identigy the hand in the given image
+code: mediapipe.solutions.hands.Hands().process(<image>)
+
+drawing_utils --- function used to draw the points on the predected image
+code: mediapipe.solutions.drawing_utils
+'''
+import cv2
+import mediapipe as mp
+import pyautogui
+from math import dist
+import SpeechRecognition as S
+from playsound import playsound
+
+mp_hand_detector = mp.solutions.hands
+mp_draw = mp.solutions.drawing_utils
+screen_width, screen_height = pyautogui.size()  # getting the screen size using pyautogui module
+
+input = cv2.VideoCapture(0)  # capturing the video using opencv module
+hand_detection = mp_hand_detector.Hands(max_num_hands=1, min_detection_confidence=0.9)
+
+def position(point_name):
+    # Get finger point number
+    point_value = mp_hand_detector.HandLandmark[point_name].value
+    # Get finger point position
+    point_position_x = int(hand_landmarks.landmark[point_value].x * screen_width)#getting finger point x cordinates
+    point_position_y = int(hand_landmarks.landmark[point_value].y * screen_height)#getting finger point y cordinates
+
+    return ([point_position_x, point_position_y])
+
+def speech():
+    command=S.voice_command_processor()
+    S.executable(command)
+
+frame_count=0
+while input.isOpened():
+
+    _, image = input.read()  # reading input video from cv2 module
+    frame_height, frame_width, _ = image.shape  # getting the dimensions of the frame
+
+    image = cv2.flip(image, 1)  # flipping the image
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hand_detection.process(image)  # making hand prediction of the image
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    if results.multi_hand_landmarks:
+        hand_landmarks = results.multi_hand_landmarks[0]  # predicting landmarks of one hand only
+        mp_draw.draw_landmarks(image, hand_landmarks, mp_hand_detector.HAND_CONNECTIONS)  # drawing the points of the hand on the image
+
+        thumb_finger_tip = position("THUMB_TIP")  # getting the location of the thumb finger tip
+
+        index_finger_tip = position("INDEX_FINGER_TIP")  # getting the location of the index finger tip
+        index_finger_dip = position("INDEX_FINGER_DIP")  # getting the location of index finger dip
+
+        middle_finger_tip = position("MIDDLE_FINGER_TIP")  # getting the location of middle finger tip
+        middle_finger_dip = position("MIDDLE_FINGER_DIP")  # getting the location of middle finger dip
+
+        ring_finger_dip = position("RING_FINGER_DIP")  # getting the location of ring finger dip
+
+        pinky_finger_tip = position("PINKY_TIP")  # getting the location of pinky finger tip
+        pinky_finger_dip = position("PINKY_DIP")  # getting the location of pinky finger dip
+
+        thumb_index_dist = int(dist(index_finger_tip,thumb_finger_tip))  # calculating the distance between the thumb and index finger tip
+
+        #calculating distances for open palm
+        index_middle_dist = int(dist(middle_finger_dip, index_finger_tip))  # calculating the distance between the middle finger dip and index finger tip
+        middle_ring_dist = int(dist(ring_finger_dip, middle_finger_dip))  # calculating the distance between the ring finger dip and middle finger dip
+        ring_pinky_dist = int(dist(pinky_finger_tip, ring_finger_dip))  # calculating the distance between the ring finger dip and pinky finger tip
+
+        #calculating distances for closed palm
+        index_tip_dip_dist = int(dist(index_finger_tip, index_finger_dip))  # calculating the distance between the index finger dip and index finger tip
+        middle_tip_dip_dist = int(dist(middle_finger_tip, middle_finger_dip))  # calculating the distance between the middle finger tip and middle finger dip
+        pinky_tip_dip_dist = int(dist(pinky_finger_tip, pinky_finger_dip))  # calculating the distance between the pinky finger dip and pinky finger tip
+
+        if thumb_index_dist >= 50 and thumb_index_dist <= 180 and index_middle_dist >= 80:  # if the distance between the thumb finger and index finger tips is less than 130 and grater than 50 decreasing the volume
+            pyautogui.press('volumedown')
+            print(thumb_index_dist)
+        elif thumb_index_dist >= 181 and thumb_index_dist <= 300 and index_middle_dist >= 81:  # if the distance between the thumb finger and index finger tips is less than 250 and grater than 130 increasing the volume
+            pyautogui.press('volumeup')
+            print(thumb_index_dist)
+        elif index_tip_dip_dist <= 50 and middle_tip_dip_dist <= 40 and pinky_tip_dip_dist <= 40 and thumb_index_dist <= 100:
+            print("closed palm")
+        elif index_middle_dist <= 80 and middle_ring_dist <= 90 and ring_pinky_dist <= 80 and thumb_index_dist >= 230 and not (pinky_tip_dip_dist <= 20):
+            print("open palm")
+            #increamenting the frame_count with 1 when there is an open palm on the screen
+            frame_count+=1
+            #activating the speech function if the frame_count equals to 5
+            if frame_count==5:
+                speech()
+                frame_count=0
+            
+
+    cv2.imshow('Hand Detection', image)  # display the video on the screen
+    if cv2.waitKey(5) & 0xFF == 27:  # close the output if we press Esc button
+        break
+
+input.release()
+cv2.destroyAllWindows()
